d2fdb2812592bb4af2875d05114e87b7423876a9
Esteban Puentes Roldan <rapuentes@uan.edu.co>
2022-04-14 15:47:11 -0500

    changed again

--- index.js
+++ index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const PORT = process.env.PORT || 5000
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(5000, () => {
-    console.log('%s listening at 5000'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at '+ PORT); // eslint-disable-line no-console
   });
 });
